b25830044ff0593ec8bcd35cd1f0cca86cc59fb5
Joshua Famiroju <joshuafamiroju@hotmail.com>
2022-08-11 12:04:02 +0100

    committed

--- index.js
+++ index.js
@@ -33,3 +33,30 @@ let controlBtn = document.getElementById("controlBtn")
 controlBtn.addEventListener("click", () => {
     window.location.href = "index.html"
 })
+
+function checker(first, second, third) {
+    if (
+        boxes[first].textContent === "X" &&
+        boxes[second].textContent === "X" &&
+        boxes[third].textContent === "X" 
+    ) {
+        disableButtonsWhenGameIsWon("Player 1")
+    } else if (
+        boxes[first].textContent === "O" &&
+        boxes[second].textContent === "O" &&
+        boxes[third].textContent === "O" 
+    ) {
+        disableButtonsWhenGameIsWon("Player 2")
+    }
+}
+
+function checkGameStatus() {
+    checker(0, 1, 2)
+    checker(3, 4, 5)
+    checker(6, 7, 8)
+    checker(0, 3, 6)
+    checker(1, 4, 7)
+    checker(2, 5, 8)
+    checker(0, 4, 8)
+    checker(2, 4, 6)
+}
